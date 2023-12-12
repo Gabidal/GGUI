f28b3b077a667885d3999628936323e85c5a4a78
Gabidal <golzar.gabriel@gmail.com>
2023-12-12 20:54:10 +0200

    shadow box

--- main.cpp
+++ main.cpp
@@ -1,32 +1,18 @@
-#include "Renderer.h"
+#include "ggui.h"
 
-using namespace std;
+using namespace GGUI;
 
+int main() 
+{
+    GGUI::GGUI([&](){    
+        GGUI::Main->Set_Title("Your App UI");
+        GGUI::Main->Set_Background_Color(COLOR::WHITE);
+        GGUI::Main->Set_Text_Color(COLOR::BLACK);
+        GGUI::Main->Show_Border(true);
 
-int main(){
-    //Pause renderer and resume renderer are only for super optimization purposes, if you dont need blazing fast TUI you can just ignore them.
-    GGUI::Window* Main = GGUI::Init_Renderer();
-    Main->Show_Border(true);
-    
-    GGUI::Window* Text = new GGUI::Window(
-        "Hello World!", 
-        20,
-        10,
+        GGUI::HTML* Test = new GGUI::HTML("Examples/simple.html");
+    });   
 
-        GGUI::COLOR::CYAN,
-        GGUI::COLOR::DARK_MAGENTA,
-
-        GGUI::COLOR::WHITE,
-        GGUI::COLOR::LIGHT_BLUE
-    );
-
-    // Center the text element
-    Text->Set_Position({Main->Get_Width()/2 - Text->Get_Width()/2, Main->Get_Height()/2 - Text->Get_Height()/2});
-    
-    Main->Add_Child(Text);
-
-    while(true){ 
-        //Your programm here :D
-    }
-
+    GGUI::SLEEP(INT32_MAX);
+    return 0;
 }
