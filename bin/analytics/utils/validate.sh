536c841ece241eb817144b937ca72bd3bb937ed7
Gabidal <Quanfoxes@gmail.com>
2025-08-27 23:08:25 +0300

    Now it should be fixed

--- bin/analytics/utils/validate.sh
+++ bin/analytics/utils/validate.sh
@@ -222,7 +222,7 @@ test_requirements() {
             ((TEST_FAILED++))
         else
             # Extract and validate G++ version (must be 13 or higher)
-            local gpp_version=$(echo "$gpp_version_output" | grep -oE '[0-9]+' | head -n1)
+            local gpp_version=$(echo "$gpp_version_output" | sed -n 's/.* \([0-9]\+\).*/\1/p')
             if [[ -n "$gpp_version" && "$gpp_version" -ge 13 ]]; then
                 echo "G++ version: SUFFICIENT (version $gpp_version)"
             else
